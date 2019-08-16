c61296aceca72413c5e18a179b296baa2dec8891
Marvin Ma <yma010@ucr.edu>
2019-08-16 16:33:19 -0700

    to_query_problems.rb ALL SPECS PASSING

--- skeleton/lib/to_query_problems.rb
+++ skeleton/lib/to_query_problems.rb
@@ -130,7 +130,19 @@ def no_apples_for_blair
 
   # DO NOT USE A SUBQUERY
   execute(<<-SQL)
-
+    SELECT
+      cats.name
+    FROM
+      cats
+    JOIN
+      cattoys ON cats.id = cat_id
+    JOIN
+      toys ON toys.id = toy_id
+    WHERE
+      toys.name = 'Apple' 
+      AND cats.name != 'Blair'
+    ORDER BY
+      cats.name;
   SQL
 end
 
@@ -139,9 +151,30 @@ def no_apples_for_blair_sub
   # Blair has was too many apple toys! Find the name of all the cats that
   # own toys named `Apple` that aren't `Blair`. 
   # Order by cat name alphabetically.
-
   # USE A SUBQUERY
+
+  # more efficent with sub query
   execute(<<-SQL)
+    SELECT
+      cats.name
+    FROM
+      cats
+    JOIN
+      cattoys ON cats.id = cat_id
+    JOIN
+      toys ON toys.id = toy_id
+    WHERE
+      cats.name != 'Blair'
+      AND toys.id IN (
+        SELECT
+          toys.id
+        FROM
+          toys
+        WHERE
+          toys.name = 'Apple'
+      )
+    ORDER BY
+      cats.name;
 
   SQL
 end
@@ -153,7 +186,18 @@ def toys_that_brendon_owns
 
   # DO NOT USE A SUBQUERY
   execute(<<-SQL)
-
+    SELECT
+      toys.name
+    FROM
+      toys
+    JOIN
+      cattoys ON toys.id = toy_id
+    JOIN
+      cats ON cats.id = cat_id
+    WHERE
+      cats.name = 'Brendon'
+    ORDER BY
+      toys.name;
   SQL
 end
 
@@ -163,7 +207,25 @@ def toys_that_brendon_owns_sub
 
   # USE A SUBQUERY
   execute(<<-SQL)
-
+    SELECT
+      toys.name
+    FROM
+      toys
+    JOIN
+      cattoys ON toys.id = toy_id
+    JOIN
+      cats ON cats.id = cat_id
+    WHERE
+      cats.id IN (
+        SELECT
+          cats.id
+        FROM
+          cats
+        WHERE
+          cats.name = 'Brendon'
+      )
+    ORDER BY
+      toys.name;
   SQL
 end
 
@@ -177,7 +239,18 @@ def price_like_shiny_mouse
 
   # DO NOT USE A SUBQUERY
   execute(<<-SQL) 
-  
+    SELECT
+      not_shiny.name, not_shiny.price
+    FROM
+      toys AS not_shiny
+    JOIN
+      toys AS shiny ON not_shiny.price = shiny.price
+    WHERE
+      not_shiny.name != 'Shiny Mouse'
+      AND shiny.name = 'Shiny Mouse'
+    ORDER BY
+      not_shiny.name;
+
   SQL
 end
 
@@ -191,7 +264,22 @@ def price_like_shiny_mouse_sub
 
   # USE A SUBQUERY
   execute(<<-SQL) 
-
+    SELECT
+      toys.name, toys.price
+    FROM
+      toys      
+    WHERE
+      toys.name != 'Shiny Mouse'
+      AND toys.price IN (
+        SELECT
+          toys.price
+        FROM
+          toys
+        WHERE
+          toys.name = 'Shiny Mouse'
+      )
+    ORDER BY
+      toys.name;
   SQL
 end
 
@@ -203,7 +291,17 @@ def just_like_orange
 
   # DO NOT USE A SUBQUERY
   execute(<<-SQL)
-
+  SELECT
+    not_orange.name, not_orange.breed
+  FROM
+    cats AS not_orange
+  JOIN
+    cats AS orange on not_orange.breed = orange.breed
+  WHERE
+    not_orange.name != 'Orange' AND
+    orange.name = 'Orange'
+  ORDER BY
+    not_orange.name;
   SQL
 end
 
@@ -215,7 +313,22 @@ def just_like_orange_sub
 
   # USE A SUBQUERY
   execute(<<-SQL)
-
+  SELECT
+    cats.name, cats.breed
+  FROM
+    cats
+  WHERE
+    cats.name != 'Orange' AND
+    cats.breed IN (
+      SELECT
+        cats.breed
+      FROM
+        cats
+      WHERE
+        cats.name = 'Orange'
+    )
+  ORDER BY
+    cats.name;
   SQL
 end
 
@@ -229,7 +342,27 @@ def toys_that_jet_owns
 
   # DO NOT USE A SUBQUERY
   execute(<<-SQL)
+  SELECT
+    not_jet.name, toys.name
+  FROM
+    toys
+  JOIN
+    cattoys AS jet_toy ON toys.id = jet_toy.toy_id
+  JOIN
+    cats AS is_jet ON is_jet.id = jet_toy.cat_id
 
+
+  JOIN
+    cattoys AS not_jet_toy ON toys.id = not_jet_toy.toy_id
+  JOIN
+    cats AS not_jet ON not_jet.id = not_jet_toy.cat_id
+  WHERE
+    is_jet.name = 'Jet' AND
+    not_jet.name != 'Jet'
+  GROUP BY
+    not_jet.name, toys.name
+  ORDER BY
+    not_jet.name;
   SQL
 end
 
@@ -241,6 +374,32 @@ def toys_that_jet_owns_sub
 
   # USE A SUBQUERY
   execute(<<-SQL)
+  SELECT
+    cats.name,
+    toys.name
+  FROM
+    toys
+  JOIN
+    cattoys on toys.id = toy_id
+  JOIN
+    cats on cats.id = cat_id
+  WHERE
+    cats.name != 'Jet'
+    AND
+    toys.id IN (
+      SELECT
+        toys.id
+      FROM
+        toys
+      JOIN
+        cattoys on toys.id = toy_id
+      JOIN
+        cats on cats.id = cat_id
+      WHERE
+        cats.name = 'Jet'
+    )
+  ORDER BY
+    cats.name;
 
   SQL
 end
